4da0bfb25e385e8df469eff04f05ddb3a057717e
Shwetanshu Shekhar <47172840+shwetanshutech@users.noreply.github.com>
2020-10-11 02:16:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    int_features = [age,sex,chest,rbp,,fbs,rer,mrh,eia,op,slope,nmv,thal]
